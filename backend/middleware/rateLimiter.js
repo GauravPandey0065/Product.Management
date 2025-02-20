3225c525b32926be3ae48c49b3c322d261c5fdda
gaurav pandey <127774454+GauravPandey0065@users.noreply.github.com>
2025-02-20 20:47:25 +0530

    Update rateLimiter.js

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,7 +1,9 @@
 import rateLimit from "express-rate-limit";
 
-export const rateLimiter = rateLimit({
-    windowMs: 60 * 60 * 1000, // 1 hour
-    max: 100,
-    message: "Too many requests, please try again later."
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 100, // Limit each IP to 100 requests per hour
+  message: "Too many requests, please try again later."
 });
+
+export default limiter;
